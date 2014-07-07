f2d15ea22176799e7929c22282976bd87daf0842
luongkt <luongkt_57@vnu.edu.vn>
2014-07-07 18:34:54 +0700

    baitap tuan1

---
+++ asg01/Student.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+/**
+ *
+ * @author Luong
+ */
+public class Student {
+
+    /**
+     * @param args the command line arguments
+     */
+
+   
+ 
+    public static void main(String[] args) {
+     System.out.println("Thông tin cá nhân của bạn là:");
+     String HT = "Kiều Thị Lương";
+     int MSSV = 12020240;
+     String lop = "k57cc";
+     String username ="luongkt";
+     String email=" luongkt_57@vnu.edu.vn";
+     System.out.println(HT+"\t" + MSSV +"\t"+ lop +"\t"+ username +"\t"+ email );
+
+     for(int i=9;i>0;i--){
+         System.out.println(i+ " bottles of beer on the wall,"+i+ "bottles of beer.");
+     System.out.println("Take one down, pass it around, ");
+     }
+      
+      System.out.println("No more bottles of beer on the wall.");
+      
+        
+       
+       
+    }
+}
